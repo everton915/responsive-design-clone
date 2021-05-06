3761e87da18e48428a102ccc258c0e35602f5285
Ty Everton <everton_915@yahoo.com>
2021-05-05 21:14:34 -0600

    second

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,7 +11,8 @@ function App() {
         <h1 class='h1'>GRAYSCALE</h1>
         <h4 class='h4'>A free, responsive, one page Bootstrap theme <br></br>created by Start Bootstrap.</h4>
         <button class='getStarted'>GET STARTED</button>
-        <header className='menuBorder'>Start Bootstrap</header>
+        <div className='menuBorder'></div>
+        <button class='mobileStartB'><b>Start Bootstrap</b></button>
     </div>
   );
 }

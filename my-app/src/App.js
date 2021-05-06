fbda76348e717762568f1d055ab616a5ecade3c3
Ty Everton <everton_915@yahoo.com>
2021-05-05 18:41:03 -0600

    first

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+        <button class='startB'>Start Bootstrap</button>
+        <button class='menu'>Contact</button>
+        <button class='menu'>Projects</button>
+        <button class='menu'>About</button>
+        <h1 class='h1'>GRAYSCALE</h1>
+        <h4 class='h4'>A free, responsive, one page Bootstrap theme <br></br>created by Start Bootstrap.</h4>
+        <button class='getStarted'>GET STARTED</button>
+        <header className='menuBorder'>Start Bootstrap</header>
+    </div>
+  );
+}
+
+export default App;
